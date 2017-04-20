ab0cd0a270936ed4d770d60a5a2d7472a9a85ee8
Tirdad Kiamanesh <tkiamanesh@id.iit.edu>
2017-04-20 14:16:31 -0500

    appended the view sun and grass to the var view

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 // Do your work in this file.
 
+//view
+
+var view = document.createElement("div");
+document.body.appendChild(view)
+
+//grass
+
+var grass = document.createElement("div");
+document.body.appendChild(view)
+
+//sun
+
+var sun = document.createElement("div");
+document.body.appendChild(view)
+
