f62475dc67be027cd13f242d832c7447fea54e86
Tirdad Kiamanesh <tkiamanesh@id.iit.edu>
2017-04-20 16:47:49 -0500

    I can not make the function show image instead of text

--- script.js
+++ script.js
@@ -2,35 +2,81 @@
 
 //view
 
-var view = document.createElement("div");
-document.body.appendChild(view)
+
+// document.body.onload = addElement;
+
+function show_image() { 
+  // create a new div element 
+  // and give it some content 
+
+    var view = document.createElement("view");
+
+    // This next line will just add it to the <body> tag
+    document.body.appendChild(view);
+
+// var view = document.createElement("div");
+// document.body.appendChild(view);
+
+//   var view = document.createElement("div"); 
+//   var newContent = document.createTextNode("Hi there and greetings!"); 
+//   view.appendChild(newContent); //add the text node to the newly created div. 
+
+//   // add the newly created element and its content into the DOM 
+//   var currentDiv = document.getElementById("div1"); 
+//   document.body.insertBefore(view, currentDiv); 
+}
+
+
+
+
+
+
+// var view = document.createElement("div");
+// document.body.appendChild(view);
+
+
 
 //grass
 
-var grass = document.createElement("div");
-document.body.appendChild(view)
+// var grass = document.createElement("div");
+// document.body.appendChild(view);
 
 //sun
 
-var sun = document.createElement("div");
-document.body.appendChild(view)
+// var sun = document.createElement("div");
+// document.body.appendChild(view);
 
-// while for ray from 1 to 5
+// // while for ray from 1 to 5
 
-var n = 0;
+// // var n = 0;
 
-while (n < 6) {
-  n++;
- ray-n;
-}
+// // while (n < 6) {
+// //   n++;
+// //  ray-n;
+// // }
 
-// mountain and mountain top 
+// // mountain and mountain top 
 
-var mountain = document.createElement("div");
-document.body.appendChild(view)
+// var mountain = document.createElement("div");
+// document.body.appendChild(view);
 
-var mountainTop = document.createElement("div");
-document.body.appendChild(view)
+// var mountainTop = document.createElement("div");
+// document.body.appendChild(view);
+
+
+// document.body.onload = addElement;
+
+// function addElement () { 
+//   // create a new div element 
+//   // and give it some content 
+//   var newDiv = document.createElement("div"); 
+//   var newContent = document.createTextNode("Hi there and greetings!"); 
+//   newDiv.appendChild(newContent); //add the text node to the newly created div. 
+
+//   // add the newly created element and its content into the DOM 
+//   var currentDiv = document.getElementById("div1"); 
+//   document.body.insertBefore(newDiv, currentDiv); 
+// }
 
 
 
