aca73d723aee2d6d79c7fdca4617531c2ad53572
Tirdad Kiamanesh <tkiamanesh@id.iit.edu>
2017-04-20 15:52:13 -0500

    mountain

--- script.js
+++ script.js
@@ -15,9 +15,23 @@ document.body.appendChild(view)
 var sun = document.createElement("div");
 document.body.appendChild(view)
 
+// while for ray from 1 to 5
+
 var n = 0;
 
 while (n < 6) {
   n++;
  ray-n;
 }
+
+// mountain and mountain top 
+
+var mountain = document.createElement("div");
+document.body.appendChild(view)
+
+var mountainTop = document.createElement("div");
+document.body.appendChild(view)
+
+
+
+
